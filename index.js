5c39120126fad37c84e276cc5f0a1d85f170d79a
Adam <39442291+Aurathic@users.noreply.github.com>
2023-02-02 17:59:12 -0500

    Add option to select judging team
    (So the team doesn't draw their own article.)
    Next step: add export/import of team/article list for use in future games

--- index.js
+++ index.js
@@ -41,9 +41,14 @@ function addAlert(alertText) {
 /**
  * Pick and remove a random article from a random team.
  */
-function pickArticle() {
+function pickArticle(judgingTeam) {
     const teams = Object.keys(articles);
-    const teamArticleList = articles[teams[Math.floor(teams.length * Math.random())]];
+    let randomTeam = judgingTeam;
+    while (randomTeam == judgingTeam) { //Don't draw an article from the judging team's set
+        randomTeam = teams[Math.floor(teams.length * Math.random())];
+    }
+    console.log(randomTeam);
+    const teamArticleList = articles[randomTeam];
     const articleIndex = Math.floor(teamArticleList.length * Math.random());
     const randomArticle = teamArticleList[articleIndex];
     teamArticleList.splice(articleIndex, 1);
@@ -74,7 +79,7 @@ $("#add-article > button").click(
         const teamNameStr = teamName.val().trim();
         const articleNameStr = articleName.val().trim();
 
-        if ((teamNameStr != missingTeam) && (teamNameStr != "")
+        if ((teamNameStr != missingTeam)
             && !(articleNameStr in articles[teamNameStr])) { // Only add article if it's not already in the list
             articles[teamNameStr].push(articleNameStr);
             console.log(articles);
@@ -86,24 +91,28 @@ $("#add-article > button").click(
 // Get Article
 $("#get-article > button").click(
     function () {
-        const displayDiv = $("#display");
-        displayDiv.empty();
+        const judgingTeam = $("#get-article > select");
+        const judgingTeamStr = judgingTeam.val();
 
-        const teams = Object.keys(articles);
-        const teamMissingArticles = checkTeamsHaveArticles();
+        if (judgingTeamStr != missingTeam) {
+            const displayDiv = $("#display");
+            displayDiv.empty();
 
+            const teams = Object.keys(articles);
+            const teamMissingArticles = checkTeamsHaveArticles();
 
-        if (teams.length < 2) {
-            // Display a warning if there aren't enough teams
-            addAlert("Add at least 2 teams before picking an article.");
-        }
-        else if (teamMissingArticles) {
-            // Add a warning to display div if some team is missing an element
-            addAlert(`Team '${teamMissingArticles}' doesn't have any articles to pick from.`);
-        } else {
-            const randomArticle = pickArticle();
-            const articleName = `<h1 id="article-text" class="text-center border">${randomArticle}</h1>`
-            displayDiv.append(articleName);
+            if (teams.length < 2) {
+                // Display a warning if there aren't enough teams
+                addAlert("Add at least 2 teams before picking an article.");
+            }
+            else if (teamMissingArticles) {
+                // Add a warning to display div if some team is missing an element
+                addAlert(`Team '${teamMissingArticles}' doesn't have any articles to pick from.`);
+            } else {
+                const randomArticle = pickArticle(judgingTeamStr);
+                const articleName = `<h1 id="article-text" class="text-center border">${randomArticle}</h1>`
+                displayDiv.append(articleName);
+            }
         }
     }
 )
